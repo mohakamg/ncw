8e2cee4a091830f524759159affee41d8774084a
mohakamg <mohak.kant1amg@gmail.com>
2017-04-04 15:23:37 -0500

    Designing...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Student.create(username: "mkant", password_digest: "0203-2748")
+Teacher.create(first_name: "Dummy", last_name: "Teacher", email: "dummy.teacher@gmail.com", phone: "6507418802", password: "0203-2748", password_confirmation: "0203-2748", country: "United States Of America", gender: "Male", email_confirmed: true, acceptance_of_terms: true)
