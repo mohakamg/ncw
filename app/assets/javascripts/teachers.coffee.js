b5447f723f626ca8211deb64a8b746293d4f6f75
mohakamg <mohak.kant1amg@gmail.com>
2017-04-03 20:15:55 -0500

    Designing

--- app/assets/javascripts/teachers.coffee.js
+++ app/assets/javascripts/teachers.coffee.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
   }, 1000, "linear");
 
   for(var i=0;i<$('.card').length;i++){
-    $("#teacher-card-" + (i+1).toString()).animate({
-      opacity: 1
-    }, 1000, "linear");
-  }
+    setTimeout(function() {
+      $("#teacher-card-" + (i+1).toString()).animate({
+        opacity: 1
+      }, 1000, "linear");
+    }
+  }, 1000);
+  //your code to be executed after 1 second
 
 });
